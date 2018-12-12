ee9e808f4a05fc215c7b4bdc12fdd28b86f07fd2
github username <github email address>
2018-12-12 11:41:10 -0600

    Make the category info beautiful

--- src/components/CategoryInfo.js
+++ src/components/CategoryInfo.js
@@ -1,16 +1,27 @@
 import React from 'react';
-
+import { Grid } from 'semantic-ui-react'
 const CategoryInfo = ({ category }) => {
     return (
         <>
             <div
                 className="category-info"
             >
-                <div className="category-thumbnail" style={{ backgroundImage: `url(${category['img_url']})` }}>
-                </div>
-                <span>{category.cards.length} cards</span>
-                <h1>{category.name}</h1>
-                <p>{category.desc}</p>
+                <Grid>
+                    <Grid.Row>
+                        <span>{category.cards.length} cards</span>
+                    </Grid.Row>
+                    <Grid.Row columns={12} divided>
+                        <Grid.Column width={2}>
+                            <div className="category-thumbnail" style={{ backgroundImage: `url(${category['img_url']})` }}></div>
+                        </Grid.Column>
+                        <Grid.Column width={10} textAlign="left" verticalAlign="middle">
+                            <h1>{category.name}</h1>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <p>{category.desc}</p>
+                    </Grid.Row>
+                </Grid>
             </div>
         </>
     )
